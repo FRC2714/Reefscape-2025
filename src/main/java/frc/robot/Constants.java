e25fe0ebb63365cfdfd3355017c5b1dce708f5c1
Kevin Liu <kevin.liu@team2714.com>
2025-01-07 20:37:11 -0600

    Added limelight helpers

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -99,4 +99,8 @@ public final class Constants {
   public static final class NeoMotorConstants {
     public static final double kFreeSpeedRpm = 5676;
   }
+
+  public static final class LimelightConstants{
+    public static final String kLimelightName = ""; //idk the name yet
+  }
 }
