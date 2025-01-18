10651307d55776479a3d6c8b1f2cbc6c6e3414a8
Kamran Nag <kamrann2353@gmail.com>
2025-01-17 20:56:23 -0600

    LL constants change

--- src/main/java/frc/robot/commands/AlignToCoral.java
+++ src/main/java/frc/robot/commands/AlignToCoral.java
@@ -44,7 +44,7 @@ public class AlignToCoral extends Command {
     
     addRequirements(m_drivetrain);
 
-    xController.setSetpoint(Units.inchesToMeters(8));
+    xController.setSetpoint(Units.inchesToMeters(13));
     yController.setSetpoint(0);
     thetaController.setSetpoint(0);
     thetaController.enableContinuousInput(-180, 180);
