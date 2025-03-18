0366ce1f88c288608c5cfcea23b5f753ce0b8c88
Kamran Nag <91856362+kjn01@users.noreply.github.com>
2025-03-17 22:35:11 -0500

    Tested fixes

--- src/main/java/frc/robot/subsystems/StateMachine.java
+++ src/main/java/frc/robot/subsystems/StateMachine.java
@@ -334,7 +334,7 @@ public class StateMachine extends SubsystemBase {
   public Command dragonScoreL4Sequence() {
     return m_dragon
         .scoreReadyLevel(DragonSetpoint.L4)
-        .until(() -> !m_dragon.isClearFromReef())
+        .until(m_dragon::isClearToScoreL4)
         .andThen(m_dragon.score().until(() -> !m_dragon.isCoralOnDragon()))
         .andThen(stopScore())
         .beforeStarting(() -> m_state = State.DRAGON_SCORE);
