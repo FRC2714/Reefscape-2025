aed18dd0ef1e3c47022ce6fdb43b154269d72c2a
Kamran Nag <91856362+kjn01@users.noreply.github.com>
2025-05-03 11:09:31 -0500

    final spotless!!
    Co-Authored-By: Ryan Juramongkol <51488420+ryanju0@users.noreply.github.com>
    Co-Authored-By: Kevin Liu <128659871+kvine11@users.noreply.github.com>
    Co-Authored-By: Hari Anoop <105997075+hanoopgit@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/Dragon.java
+++ src/main/java/frc/robot/subsystems/Dragon.java
@@ -4,11 +4,7 @@
 
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.Millimeters;
-
 import au.grapplerobotics.LaserCan;
-import au.grapplerobotics.interfaces.LaserCanInterface;
-import au.grapplerobotics.interfaces.LaserCanInterface.Measurement;
 import au.grapplerobotics.interfaces.LaserCanInterface.RangingMode;
 import au.grapplerobotics.interfaces.LaserCanInterface.RegionOfInterest;
 import au.grapplerobotics.interfaces.LaserCanInterface.TimingBudget;
@@ -36,7 +32,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Configs;
 import frc.robot.Constants.DragonConstants;
-import frc.robot.Constants.DragonConstants.LaserCanConstants;
 import frc.robot.Constants.DragonConstants.PivotSetpoints;
 import frc.robot.Constants.DragonConstants.RollerSetpoints;
 import frc.robot.Constants.SimulationRobotConstants;
